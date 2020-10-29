d541a87b018b517038984b0a45500e62b7f82afb
unknown <Lamine HARRAT@SHIDG001.shinformatique.local>
2020-10-29 21:19:19 +0100

    ajout du fichier forgotPassword

--- resources/views/auth/forgotPassword.blade.php
+++ resources/views/auth/forgotPassword.blade.php
@@ -1,6 +1,6 @@
 @extends('auth.layout')
 
-@section('authtitle', 'Initialisation du mot de passe')
+@section('authtitle', 'Mot de passe oublié')
 
 @section('form')
 <form action="index.html">
@@ -11,14 +11,6 @@
    <div class="mt-4">
       <button type="submit" class="btn btn-outline-primary btn-block btn-lg">Envoyer</button>
    </div>
-   <div class="form-group">
-        <label>Mot de passe</label>
-        <input type="text" name="password" class="form-control" placeholder="Entrer votre mot de passe">
-    </div>
-    <div class="form-group">
-        <label>Confirmation du mot de passe</label>
-        <input type="text" name="password_confirmation" class="form-control" placeholder="Confirmer votre mot de passe">
-    </div>
 </form>
 <div class="text-center mt-5">
 <p class="light-gray">Vous n'avez pas encore de compte ?<a href="register.html">S'inscrire</a></p>
