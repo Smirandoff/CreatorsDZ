5f864155a5398e4ce8fa741f23e3096811360c12
unknown <Lamine HARRAT@SHIDG001.shinformatique.local>
2020-10-29 14:25:00 +0100

    ajout des fichiers login register et forget password

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,69 @@
+@extends('layout.layout')
+
+@section('title', 'Se connecter')
+
+@section('content')
+
+            <div class="row no-gutters">
+               <div class="col-md-5 p-5 bg-white full-height">
+                    <div class="login-main-left">
+                        <div class="text-center mb-5 login-main-left-header pt-4">
+                            <img src="img/favicon.png" class="img-fluid" alt="LOGO">
+                            <h5 class="mt-3 mb-3">Welcome to Vidoe</h5>
+                            <p>It is a long established fact that a reader <br> will be distracted by the readable.</p>
+                        </div>
+                        <form action="index.html">
+                            <div class="form-group">
+                                <label>Adresse mail</label>
+                                <input type="text" name="email" class="form-control" placeholder="Entrer l'adresse mail">
+                            </div>
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="password" name="password" class="form-control" placeholder="Entrer votre mot de passe">
+                            </div>
+                            <div class="form-group custom-control custom-checkbox">
+                                <input type="checkbox" name='remember' class="custom-control-input" id="customCheck1">
+                                <label class="custom-control-label" for="customCheck1">Se souvenir du mot de passe</label>
+                            </div>
+                            <div class="mt-4">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <button type="submit" class="btn btn-outline-primary btn-block btn-lg">Se connecter</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="text-center mt-5">
+                            <p class="light-gray">Don’t have an account? <a href="register.html">Sign Up</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="login-main-right bg-white p-5 mt-5 mb-5">
+                        <div class="owl-carousel owl-carousel-login">
+                            <div class="item">
+                                <div class="carousel-login-card text-center">
+                                    <img src="img/login.png" class="img-fluid" alt="LOGO">
+                                    <h5 class="mt-5 mb-3">​Watch videos offline</h5>
+                                    <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="carousel-login-card text-center">
+                                    <img src="img/login.png" class="img-fluid" alt="LOGO">
+                                    <h5 class="mt-5 mb-3">Download videos effortlessly</h5>
+                                    <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="carousel-login-card text-center">
+                                    <img src="img/login.png" class="img-fluid" alt="LOGO">
+                                    <h5 class="mt-5 mb-3">Create GIFs</h5>
+                                    <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+@endsection
