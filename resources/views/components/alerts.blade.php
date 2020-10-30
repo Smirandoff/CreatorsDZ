a373c27d8320b9dd130cdd7dcece777fca47c891
Zemmouri Rediane <z.rediane24@gmail.com>
2020-10-30 16:00:24 +0100

    Add alert for error flashed session data

--- resources/views/components/alerts.blade.php
+++ resources/views/components/alerts.blade.php
@@ -3,12 +3,17 @@
     <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
     @if($errors->any())
     <div class="alert alert-danger" role="alert">
-        Votre soumission contient des erreurs, veuillez les corriger ! 
+        Votre soumission contient des erreurs, veuillez les corriger !
     </div>
     @endif
     @if(session()->has('success'))
     <div class="alert alert-success" role="alert">
         {{session()->get('success')}}
+    </div>
+    @endif
+    @if(session()->has('error'))
+    <div class="alert alert-danger" role="alert">
+        {{ session()->get('error') }}
     </div>
     @endif
 </div>
