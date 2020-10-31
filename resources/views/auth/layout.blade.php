539f7c515a87f508abb6066a0300d711da5de105
unknown <Lamine HARRAT@SHIDG001.shinformatique.local>
2020-10-31 09:53:44 +0100

    modification layoutauth

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -8,7 +8,7 @@
                     <div class="col-md-5 p-5 bg-white full-height">
                         <div class="login-main-left">
                             <div class="text-center mb-5 login-main-left-header pt-4">
-                                <img src="img/favicon.png" class="img-fluid" alt="LOGO">
+                                <img src="{{asset('img/favicon.png')}}" class="img-fluid" alt="LOGO">
                                 <h5 class="mt-3 mb-3">Bienvenue à DZCreators</h5>
                                 <p>It is a long established fact that a reader <br> will be distracted by the readable.</p>
                             </div>
@@ -21,21 +21,21 @@
                             <div class="owl-carousel owl-carousel-login">
                                 <div class="item">
                                     <div class="carousel-login-card text-center">
-                                        <img src="img/login.png" class="img-fluid" alt="LOGO">
+                                        <img src="{{asset('img/login.png')}}" class="img-fluid" alt="LOGO">
                                         <h5 class="mt-5 mb-3">​Watch videos offline</h5>
                                         <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
                                     </div>
                                 </div>
                                 <div class="item">
                                     <div class="carousel-login-card text-center">
-                                        <img src="img/login.png" class="img-fluid" alt="LOGO">
+                                        <img src="{{asset('img/login.png')}}" class="img-fluid" alt="LOGO">
                                         <h5 class="mt-5 mb-3">Download videos effortlessly</h5>
                                         <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
                                     </div>
                                 </div>
                                 <div class="item">
                                     <div class="carousel-login-card text-center">
-                                        <img src="img/login.png" class="img-fluid" alt="LOGO">
+                                        <img src="{{asset('img/login.png')}}" class="img-fluid" alt="LOGO">
                                         <h5 class="mt-5 mb-3">Create GIFs</h5>
                                         <p class="mb-4">when an unknown printer took a galley of type and scrambled<br> it to make a type specimen book. It has survived not <br>only five centuries</p>
                                     </div>
