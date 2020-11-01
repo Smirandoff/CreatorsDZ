4d277b736866e74221667d87862cc7e3b5712701
Zemmouri Rediane <z.rediane24@gmail.com>
2020-11-01 23:19:22 +0100

    Remove postal code and town from profile edition form

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -60,23 +60,6 @@
         <div class="row">
             <div class="col-sm-6">
                 <div class="form-group">
-                    <label class="control-label">Ville <span class="required"></span></label>
-                    <select  class="custom-select">
-                        <option value="">Selectionner une ville</option>
-                        <option value="DZ">Alger</option>
-                    </select>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="form-group">
-                    <label class="control-label">Code postal<span class="required"></span></label>
-                    <input name="banque_poste" class="form-control border-form-control" value="{{}}" placeholder="00000" type="number">
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="form-group">
                     <label class="control-label">Banque/Poste<span class="required"></span></label>
                     <input name="banque_poste" class="form-control border-form-control" value="{{}}" placeholder="Algérie poste" type="text">
                 </div>
