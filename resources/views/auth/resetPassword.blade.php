116c0df0eeef3737092065f00d914069ee27976d
Zemmouri Rediane <z.rediane24@gmail.com>
2020-11-02 12:37:22 +0100

    Fix reset password frontend

--- resources/views/auth/resetPassword.blade.php
+++ resources/views/auth/resetPassword.blade.php
@@ -3,24 +3,41 @@
 @section('authtitle', 'Initialisation du mot de passe')
 
 @section('form')
-<form action="index.html">
+<form method="POST" action="/reset-password">
+   @csrf
+   <input type="hidden" name="token" value="{{request()->route('token')}}">
    <div class="form-group">
       <label>Adresse mail</label>
-      <input type="text" name="email" class="form-control" placeholder="Entrer l'adresse mail">
+      <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Entrer l'adresse mail">
+      @error('email')
+      <div class="invalid-feedback">
+         {{$message}}
+      </div>
+      @enderror
+   </div>
+   <div class="form-group">
+      <label>Mot de passe</label>
+      <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Entrer votre mot de passe">
+      @error('password')
+      <div class="invalid-feedback">
+         {{$message}}
+      </div>
+      @enderror
+   </div>
+   <div class="form-group">
+      <label>Confirmation du mot de passe</label>
+      <input type="password" name="password_confirmation" class="form-control @error('password') is-invalid @enderror" placeholder="Confirmer votre mot de passe">
+      @error('password_confirmation')
+      <div class="invalid-feedback">
+         {{$message}}
+      </div>
+      @enderror
    </div>
    <div class="mt-4">
       <button type="submit" class="btn btn-outline-primary btn-block btn-lg">Envoyer</button>
    </div>
-   <div class="form-group">
-        <label>Mot de passe</label>
-        <input type="text" name="password" class="form-control" placeholder="Entrer votre mot de passe">
-    </div>
-    <div class="form-group">
-        <label>Confirmation du mot de passe</label>
-        <input type="text" name="password_confirmation" class="form-control" placeholder="Confirmer votre mot de passe">
-    </div>
 </form>
 <div class="text-center mt-5">
-<p class="light-gray">Vous n'avez pas encore de compte ?<a href="register.html">S'inscrire</a></p>
+   <p class="light-gray">Vous n'avez pas encore de compte ?<a href="register.html">S'inscrire</a></p>
 </div>
-@endsection         
+@endsection
