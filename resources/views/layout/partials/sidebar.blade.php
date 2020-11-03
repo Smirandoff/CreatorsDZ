b4608e7b8a5045ae229ce0de99d0bdbd902e952e
unknown <Lamine HARRAT@SHIDG001.shinformatique.local>
2020-11-03 22:45:37 +0100

    modification sur le sidebar

--- resources/views/layout/partials/sidebar.blade.php
+++ resources/views/layout/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<ul class="sidebar navbar-nav navbar-expand-sm">
+<ul class="sidebar navbar-nav navbar-expand-lg">
   <li class="nav-item active">
      <a class="nav-link" href="index.html">
      <i class="fas fa-fw fa-home"></i>
